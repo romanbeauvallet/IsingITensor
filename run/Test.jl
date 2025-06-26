03de0d7350e8b77814290c17c828ee649388de5b
Roman Beauvallet <roman.beauvallet@ens-lyon.fr>
2025-06-26 10:38:32 -0400

    First test

--- run/Test.jl
+++ run/Test.jl
@@ -1,2 +1,14 @@
+#!usr/bin/env julia
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
+
 using IsingITensor
 
+######## Paramètres ###########
+
+N = 20 
+s = 1/2 
+type = ["up"]
+
+####### Test ###########
+
+@show size.(initnewmps(N, s, type))
--- src/IsingITensor.jl
+++ src/IsingITensor.jl
@@ -1,6 +1,9 @@
+#!usr/bin/env julia
 module IsingITensor
 
 greet() = print("Hello World!")
 include("Librairy.jl")
+
+export hilbert, initnewmps
 
 end # module IsingITensor
